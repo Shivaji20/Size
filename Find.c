cab6a269d513441128c76b6f7cf5ca5329b86202
Shivaji20 <83912707+Shivaji20@users.noreply.github.com>
2021-05-11 21:17:29 +0530

    Created Find.c
    This is my first program on Github

---
+++ Find.c
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    // Printing size of Basic Data Types
+    cout << "Size of a Character (char) = " << sizeof(char) << " bytes" << endl;
+    cout << "Size of an Integer (int) = " << sizeof(int) << " bytes" << endl;
+    cout << "Size of a Floating Point (float) = " << sizeof(float) << " bytes" << endl;
+    cout << "Size of Double (double) = " << sizeof(double) << " bytes" << endl;
+ 
+    return 0;
+}
